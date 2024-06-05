cf607138c564b527c795cc18c2093103cf2511d8
IvanDanilovFD03 <ivd@empat.tech>
2024-06-05 20:50:51 +0300

    feat: create types for cards

---
+++ src/types/api.d.ts
@@ -0,0 +1,20 @@
+export type ProceduralTrainerCardType = {
+  id: string | number;
+  percentage: number;
+  title: string;
+  subtitle: string;
+  image: string;
+  links: {
+    flyLink: string;
+    starLink: string;
+  };
+};
+
+export type RecentProceduresCardType = {
+  id: string | number;
+  А;
+  title: string;
+  subtitle: string;
+  stars: number;
+  image?: string;
+};
