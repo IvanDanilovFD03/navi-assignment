fae59e54c9901a41c3557d9dacde77a02a7b3d3f
IvanDanilovFD03 <ivd@empat.tech>
2024-06-07 15:21:41 +0300

    feat: update MyAccountPage with ProceduralTrainerSection

--- src/pages/MyAccountPage/index.tsx
+++ src/pages/MyAccountPage/index.tsx
@@ -1,13 +1,18 @@
 import { FC } from "react";
 
 import { Container } from "UI/Container";
+import { ProceduralTrainerSection } from "modules/ProceduralTrainer";
 
 import { styles } from "./styles";
 
 interface MyAccountPageProps {}
 
 const MyAccountPage: FC<MyAccountPageProps> = () => {
-  return <Container sx={styles.root}></Container>;
+  return (
+    <Container sx={styles.root}>
+      <ProceduralTrainerSection />
+    </Container>
+  );
 };
 
 export default MyAccountPage;
