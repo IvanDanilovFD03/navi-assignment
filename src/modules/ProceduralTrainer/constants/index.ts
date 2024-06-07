39710eda6429f16e50d0b06e8439e021952c391c
IvanDanilovFD03 <ivd@empat.tech>
2024-06-07 15:20:59 +0300

    feat: create dummy data for ProceduralTrainer module

---
+++ src/modules/ProceduralTrainer/constants/index.ts
@@ -0,0 +1,27 @@
+import ProceduralImage1 from "assets/images/procedural-image-1.png";
+import ProceduralImage2 from "assets/images/procedural-image-2.png";
+import ProceduralImage3 from "assets/images/procedural-image-3.png";
+
+export const DUMMY_TRAINING_CARDS_DATA = [
+  {
+    id: 1,
+    title: "Emergency Procedures",
+    subtitle: "some sub heading here",
+    percentage: 50,
+    image: ProceduralImage1,
+  },
+  {
+    id: 2,
+    title: "Pre-Flight Procedures",
+    subtitle: "some sub heading here",
+    percentage: 0,
+    image: ProceduralImage2,
+  },
+  {
+    id: 3,
+    title: "CFI",
+    subtitle: "some sub heading here",
+    percentage: 100,
+    image: ProceduralImage3,
+  },
+];
