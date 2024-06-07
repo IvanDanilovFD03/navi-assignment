1f3ded58d6403a0de3a0a2f9c8abd00c05bd5f45
IvanDanilovFD03 <ivd@empat.tech>
2024-06-07 15:15:04 +0300

    feat: add SvgIcon from MUI and add some logic with styles for it

---
+++ src/UI/SvgIcon/styles.ts
@@ -0,0 +1,27 @@
+import { basicTheme } from "theme";
+import { SxStyles } from "types/styles";
+
+export const styles: SxStyles = {
+  small: {
+    "&": {
+      width: "12px",
+      height: "12px",
+    },
+  },
+  medium: {
+    "&": {
+      width: "16px",
+      height: "16px",
+    },
+  },
+  large: {
+    "&": {
+      width: "50px",
+      height: "50px",
+    },
+  },
+  huge: {
+    width: 120,
+    height: 120,
+  },
+};
