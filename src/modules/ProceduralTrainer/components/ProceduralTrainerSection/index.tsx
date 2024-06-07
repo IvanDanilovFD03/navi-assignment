f87076a8bf1a6c1f53886efa2410eb9422e1b061
IvanDanilovFD03 <ivd@empat.tech>
2024-06-07 15:21:13 +0300

    feat: create ProceduralTrainerSection component

---
+++ src/modules/ProceduralTrainer/components/ProceduralTrainerSection/index.tsx
@@ -0,0 +1,33 @@
+import React, { FC } from "react";
+
+import { Box } from "UI/Box";
+import { Typography } from "UI/Typography";
+import { TrainerCardsList } from "../TrainerCardsList";
+
+import { styles } from "./styles";
+
+import { DUMMY_TRAINING_CARDS_DATA } from "modules/ProceduralTrainer/constants";
+import {
+  getDayName,
+  getDayOfMonth,
+  getMonthName,
+} from "modules/ProceduralTrainer/helpers";
+
+export interface ProceduralTrainerSectionProps {}
+
+export const ProceduralTrainerSection: FC<ProceduralTrainerSectionProps> =
+  React.memo(() => {
+    const date = new Date();
+
+    const formattedDate = `${getDayName(date).toUpperCase()}, ${getMonthName(
+      date
+    )} ${getDayOfMonth(date)}`;
+
+    return (
+      <Box sx={styles.root}>
+        <Typography sx={styles.subtitle}>{formattedDate}</Typography>
+        <Typography sx={styles.title}>Procedural Trainer</Typography>
+        <TrainerCardsList cardsList={DUMMY_TRAINING_CARDS_DATA} />
+      </Box>
+    );
+  });
