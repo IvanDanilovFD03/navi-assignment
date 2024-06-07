82a151f7aa3b1c9b9d057a3194614d8a9f6ff33b
IvanDanilovFD03 <ivd@empat.tech>
2024-06-07 15:18:53 +0300

    feat: create navigationMenuList constant

---
+++ src/modules/NavigationMenu/constants/index.tsx
@@ -0,0 +1,13 @@
+import AccountIcon from "assets/icons/account-icon.png";
+
+export const navigationMenuList = [
+  { id: 1, icon: AccountIcon, route: "/My-Account", title: "My Account" },
+  {
+    id: 2,
+    icon: AccountIcon,
+    route: "/Emergency-Procedures",
+    title: "Emergency Procedures",
+  },
+  { id: 3, icon: AccountIcon, route: "/Scenarios", title: "Scenarios" },
+  { id: 4, icon: AccountIcon, route: "/CFI", title: "CFI" },
+];
