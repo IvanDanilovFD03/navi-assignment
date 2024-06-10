f9b54f027b5d36a86efa5fa1258682f9601554f8
IvanDanilovFD03 <ivd@empat.tech>
2024-06-11 01:18:30 +0300

    feat: create dummy data for RecentProcedures module

---
+++ src/modules/RecentProcedures/constants/index.ts
@@ -0,0 +1,28 @@
+import { RecentProceduresCardType } from "types/api";
+
+export const DUMMY_RECENT_CARDS_DATA: RecentProceduresCardType[] = [
+  {
+    id: 1,
+    title: "Engine Out",
+    subtitle: "During Flight",
+    stars: 1,
+  },
+  {
+    id: 2,
+    title: "Engine Out",
+    subtitle: "During Flight",
+    stars: 2,
+  },
+  {
+    id: 3,
+    title: "Engine Out",
+    subtitle: "During Flight",
+    stars: 2,
+  },
+  {
+    id: 4,
+    title: "Engine Out",
+    subtitle: "During Flight",
+    stars: 1,
+  },
+];
