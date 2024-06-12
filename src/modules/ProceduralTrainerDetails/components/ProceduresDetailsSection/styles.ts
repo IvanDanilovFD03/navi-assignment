7d4d6d0c0b6f0d9045a69dd013b4ce68c4969f46
IvanDanilovFD03 <ivd@empat.tech>
2024-06-12 17:22:07 +0300

    feat: update styles of ProceduresDetailsSection

--- src/modules/ProceduralTrainerDetails/components/ProceduresDetailsSection/styles.ts
+++ src/modules/ProceduralTrainerDetails/components/ProceduresDetailsSection/styles.ts
@@ -4,15 +4,12 @@ import { SxStyles } from "types/styles";
 export const styles: SxStyles = {
   root: {
     display: "flex",
-    flexDirection: "column",
+    flexWrap: "wrap",
+    justifyContent: "center",
     width: "100%",
     alignItems: "center",
     rowGap: 5,
     columnGap: 5,
-    [basicTheme.breakpoints.up("tablet")]: {
-      flexDirection: "row",
-      justifyContent: "space-between",
-    },
   },
   title: {
     mt: 8,
