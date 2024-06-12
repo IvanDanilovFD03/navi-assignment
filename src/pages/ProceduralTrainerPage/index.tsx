c9c578716bbc6c55f5be80ee0e4c31eccf4146e0
IvanDanilovFD03 <ivd@empat.tech>
2024-06-12 17:05:31 +0300

    feat: update ProceduralTrainerPage with ProceduresDetailsSection

--- src/pages/ProceduralTrainerPage/index.tsx
+++ src/pages/ProceduralTrainerPage/index.tsx
@@ -1,13 +1,18 @@
 import { FC } from "react";
 
 import { Container } from "UI/Container";
+import { ProceduresDetailsSection } from "modules/ProceduralTrainerDetails";
 
 import { styles } from "./styles";
 
 interface ProceduralTrainerPageProps {}
 
 const ProceduralTrainerPage: FC<ProceduralTrainerPageProps> = () => {
-  return <Container sx={styles.root}></Container>;
+  return (
+    <Container sx={styles.root}>
+      <ProceduresDetailsSection />
+    </Container>
+  );
 };
 
 export default ProceduralTrainerPage;
--- src/pages/ProceduralTrainerPage/styles.ts
+++ src/pages/ProceduralTrainerPage/styles.ts
@@ -2,5 +2,14 @@ import { basicTheme } from "theme";
 import { SxStyles } from "types/styles";
 
 export const styles: SxStyles = {
-  root: {},
+  root: {
+    pt: 25,
+    bgcolor: basicTheme.palette.grayPalette.graySecondary,
+    height: "100%",
+    [basicTheme.breakpoints.up("large")]: {
+      pt: 35,
+      // height: "100vh",
+      // maxHeight: "100vh",
+    },
+  },
 };
