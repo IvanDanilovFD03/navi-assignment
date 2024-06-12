f35457f09574c53f97535c745290b6ef28aa16d2
IvanDanilovFD03 <ivd@empat.tech>
2024-06-12 17:03:30 +0300

    feat: add DUMMY_PROGRESS_DATA of ProceduresDetails module

---
+++ src/modules/ProceduralTrainerDetails/constants/index.ts
@@ -0,0 +1,380 @@
+export const DUMMY_PROGRESS_DATA = [
+  {
+    id: "1",
+    title: "Emergency Procedures",
+    progress: [
+      {
+        id: "11",
+        locked: false,
+        title: "Engine Out",
+        subtitle: "In Flight",
+        stars: 2,
+        percentage: 60,
+        description: `
+          <p>
+            <b>Lorem ipsum dolor sit amet,</b> consectetur adipiscing elit. Cras
+            eu posuere mi, ac ultrices elit. Suspendisse eu neque nec orci
+            pellentesque accumsan.
+          </p>
+          <ul>
+            <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+            <li>Aliquam tincidunt mauris eu risus.</li>
+            <li>Vestibulum auctor dapibus neque.</li>
+            <li>Nunc dignissim risus id metus.</li>
+          </ul>
+          <p>
+            Ut euismod eu velit imperdiet fermentum. Fusce laoreet mi vitae ante
+            posuere ullamcorper.
+          </p>
+          <p>
+            Cras vel orci quam. Duis viverra libero quis magna finibus euismod.
+            Suspendisse potenti. Pellentesque sit amet tempor orci. Praesent
+            elementum lacus et sapien porttitor, et tincidunt turpis egestas.
+            Curabitur congue tincidunt velit, quis rhoncus turpis dapibus at.
+          </p>`,
+      },
+      {
+        id: "12",
+        locked: false,
+        title: "Engine Fire",
+        subtitle: "In Flight",
+        stars: 1,
+        percentage: 12,
+        description: `
+          <p>
+            <b>Lorem ipsum dolor sit amet,</b> consectetur adipiscing elit. Cras
+            eu posuere mi, ac ultrices elit. Suspendisse eu neque nec orci
+            pellentesque accumsan.
+          </p>
+          <ul>
+            <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+            <li>Aliquam tincidunt mauris eu risus.</li>
+            <li>Vestibulum auctor dapibus neque.</li>
+            <li>Nunc dignissim risus id metus.</li>
+          </ul>
+          <p>
+            Ut euismod eu velit imperdiet fermentum. Fusce laoreet mi vitae ante
+            posuere ullamcorper.
+          </p>
+          <p>
+            Cras vel orci quam. Duis viverra libero quis magna finibus euismod.
+            Suspendisse potenti. Pellentesque sit amet tempor orci. Praesent
+            elementum lacus et sapien porttitor, et tincidunt turpis egestas.
+            Curabitur congue tincidunt velit, quis rhoncus turpis dapibus at.
+          </p>`,
+      },
+      {
+        id: "13",
+        locked: true,
+        title: "Electrical Fire",
+        subtitle: "In Flight",
+        stars: 1,
+        percentage: 0,
+        description: `
+          <p>
+            <b>Lorem ipsum dolor sit amet,</b> consectetur adipiscing elit. Cras
+            eu posuere mi, ac ultrices elit. Suspendisse eu neque nec orci
+            pellentesque accumsan.
+          </p>
+          <ul>
+            <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+            <li>Aliquam tincidunt mauris eu risus.</li>
+            <li>Vestibulum auctor dapibus neque.</li>
+            <li>Nunc dignissim risus id metus.</li>
+          </ul>
+          <p>
+            Ut euismod eu velit imperdiet fermentum. Fusce laoreet mi vitae ante
+            posuere ullamcorper.
+          </p>
+          <p>
+            Cras vel orci quam. Duis viverra libero quis magna finibus euismod.
+            Suspendisse potenti. Pellentesque sit amet tempor orci. Praesent
+            elementum lacus et sapien porttitor, et tincidunt turpis egestas.
+            Curabitur congue tincidunt velit, quis rhoncus turpis dapibus at.
+          </p>`,
+      },
+      {
+        id: "14",
+        locked: true,
+        title: "Electrical Fire",
+        subtitle: "In Flight",
+        stars: 1,
+        percentage: 0,
+        description: `
+          <p>
+            <b>Lorem ipsum dolor sit amet,</b> consectetur adipiscing elit. Cras
+            eu posuere mi, ac ultrices elit. Suspendisse eu neque nec orci
+            pellentesque accumsan.
+          </p>
+          <ul>
+            <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+            <li>Aliquam tincidunt mauris eu risus.</li>
+            <li>Vestibulum auctor dapibus neque.</li>
+            <li>Nunc dignissim risus id metus.</li>
+          </ul>
+          <p>
+            Ut euismod eu velit imperdiet fermentum. Fusce laoreet mi vitae ante
+            posuere ullamcorper.
+          </p>
+          <p>
+            Cras vel orci quam. Duis viverra libero quis magna finibus euismod.
+            Suspendisse potenti. Pellentesque sit amet tempor orci. Praesent
+            elementum lacus et sapien porttitor, et tincidunt turpis egestas.
+            Curabitur congue tincidunt velit, quis rhoncus turpis dapibus at.
+          </p>`,
+      },
+    ],
+  },
+  {
+    id: "2",
+    title: "Pre-Flight Procedures",
+    progress: [
+      {
+        id: "21",
+        locked: false,
+        title: "Engine Out",
+        subtitle: "In Flight",
+        stars: 1,
+        percentage: 41,
+        description: `
+          <p>
+            <b>Lorem ipsum dolor sit amet,</b> consectetur adipiscing elit. Cras
+            eu posuere mi, ac ultrices elit. Suspendisse eu neque nec orci
+            pellentesque accumsan.
+          </p>
+          <ul>
+            <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+            <li>Aliquam tincidunt mauris eu risus.</li>
+            <li>Vestibulum auctor dapibus neque.</li>
+            <li>Nunc dignissim risus id metus.</li>
+          </ul>
+          <p>
+            Ut euismod eu velit imperdiet fermentum. Fusce laoreet mi vitae ante
+            posuere ullamcorper.
+          </p>
+          <p>
+            Cras vel orci quam. Duis viverra libero quis magna finibus euismod.
+            Suspendisse potenti. Pellentesque sit amet tempor orci. Praesent
+            elementum lacus et sapien porttitor, et tincidunt turpis egestas.
+            Curabitur congue tincidunt velit, quis rhoncus turpis dapibus at.
+          </p>`,
+      },
+      {
+        id: "22",
+        locked: true,
+        title: "Engine Fire",
+        subtitle: "In Flight",
+        stars: 1,
+        percentage: 0,
+        description: `
+          <p>
+            <b>Lorem ipsum dolor sit amet,</b> consectetur adipiscing elit. Cras
+            eu posuere mi, ac ultrices elit. Suspendisse eu neque nec orci
+            pellentesque accumsan.
+          </p>
+          <ul>
+            <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+            <li>Aliquam tincidunt mauris eu risus.</li>
+            <li>Vestibulum auctor dapibus neque.</li>
+            <li>Nunc dignissim risus id metus.</li>
+          </ul>
+          <p>
+            Ut euismod eu velit imperdiet fermentum. Fusce laoreet mi vitae ante
+            posuere ullamcorper.
+          </p>
+          <p>
+            Cras vel orci quam. Duis viverra libero quis magna finibus euismod.
+            Suspendisse potenti. Pellentesque sit amet tempor orci. Praesent
+            elementum lacus et sapien porttitor, et tincidunt turpis egestas.
+            Curabitur congue tincidunt velit, quis rhoncus turpis dapibus at.
+          </p>`,
+      },
+      {
+        id: "23",
+        locked: true,
+        title: "Electrical Fire",
+        subtitle: "In Flight",
+        stars: 1,
+        percentage: 0,
+        description: `
+          <p>
+            <b>Lorem ipsum dolor sit amet,</b> consectetur adipiscing elit. Cras
+            eu posuere mi, ac ultrices elit. Suspendisse eu neque nec orci
+            pellentesque accumsan.
+          </p>
+          <ul>
+            <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+            <li>Aliquam tincidunt mauris eu risus.</li>
+            <li>Vestibulum auctor dapibus neque.</li>
+            <li>Nunc dignissim risus id metus.</li>
+          </ul>
+          <p>
+            Ut euismod eu velit imperdiet fermentum. Fusce laoreet mi vitae ante
+            posuere ullamcorper.
+          </p>
+          <p>
+            Cras vel orci quam. Duis viverra libero quis magna finibus euismod.
+            Suspendisse potenti. Pellentesque sit amet tempor orci. Praesent
+            elementum lacus et sapien porttitor, et tincidunt turpis egestas.
+            Curabitur congue tincidunt velit, quis rhoncus turpis dapibus at.
+          </p>`,
+      },
+      {
+        id: "24",
+        locked: true,
+        title: "Electrical Fire",
+        subtitle: "In Flight",
+        stars: 1,
+        percentage: 0,
+        description: `
+          <p>
+            <b>Lorem ipsum dolor sit amet,</b> consectetur adipiscing elit. Cras
+            eu posuere mi, ac ultrices elit. Suspendisse eu neque nec orci
+            pellentesque accumsan.
+          </p>
+          <ul>
+            <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+            <li>Aliquam tincidunt mauris eu risus.</li>
+            <li>Vestibulum auctor dapibus neque.</li>
+            <li>Nunc dignissim risus id metus.</li>
+          </ul>
+          <p>
+            Ut euismod eu velit imperdiet fermentum. Fusce laoreet mi vitae ante
+            posuere ullamcorper.
+          </p>
+          <p>
+            Cras vel orci quam. Duis viverra libero quis magna finibus euismod.
+            Suspendisse potenti. Pellentesque sit amet tempor orci. Praesent
+            elementum lacus et sapien porttitor, et tincidunt turpis egestas.
+            Curabitur congue tincidunt velit, quis rhoncus turpis dapibus at.
+          </p>`,
+      },
+    ],
+  },
+  {
+    id: "3",
+    title: "CFI",
+    progress: [
+      {
+        id: "31",
+        locked: false,
+        title: "Engine Out",
+        subtitle: "In Flight",
+        stars: 2,
+        percentage: 56,
+        description: `
+          <p>
+            <b>Lorem ipsum dolor sit amet,</b> consectetur adipiscing elit. Cras
+            eu posuere mi, ac ultrices elit. Suspendisse eu neque nec orci
+            pellentesque accumsan.
+          </p>
+          <ul>
+            <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+            <li>Aliquam tincidunt mauris eu risus.</li>
+            <li>Vestibulum auctor dapibus neque.</li>
+            <li>Nunc dignissim risus id metus.</li>
+          </ul>
+          <p>
+            Ut euismod eu velit imperdiet fermentum. Fusce laoreet mi vitae ante
+            posuere ullamcorper.
+          </p>
+          <p>
+            Cras vel orci quam. Duis viverra libero quis magna finibus euismod.
+            Suspendisse potenti. Pellentesque sit amet tempor orci. Praesent
+            elementum lacus et sapien porttitor, et tincidunt turpis egestas.
+            Curabitur congue tincidunt velit, quis rhoncus turpis dapibus at.
+          </p>`,
+      },
+      {
+        id: "32",
+        locked: true,
+        title: "Engine Fire",
+        subtitle: "In Flight",
+        stars: 1,
+        percentage: 0,
+        description: `
+          <p>
+            <b>Lorem ipsum dolor sit amet,</b> consectetur adipiscing elit. Cras
+            eu posuere mi, ac ultrices elit. Suspendisse eu neque nec orci
+            pellentesque accumsan.
+          </p>
+          <ul>
+            <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+            <li>Aliquam tincidunt mauris eu risus.</li>
+            <li>Vestibulum auctor dapibus neque.</li>
+            <li>Nunc dignissim risus id metus.</li>
+          </ul>
+          <p>
+            Ut euismod eu velit imperdiet fermentum. Fusce laoreet mi vitae ante
+            posuere ullamcorper.
+          </p>
+          <p>
+            Cras vel orci quam. Duis viverra libero quis magna finibus euismod.
+            Suspendisse potenti. Pellentesque sit amet tempor orci. Praesent
+            elementum lacus et sapien porttitor, et tincidunt turpis egestas.
+            Curabitur congue tincidunt velit, quis rhoncus turpis dapibus at.
+          </p>`,
+      },
+      {
+        id: "33",
+        locked: true,
+        title: "Electrical Fire",
+        subtitle: "In Flight",
+        stars: 1,
+        percentage: 0,
+        description: `
+          <p>
+            <b>Lorem ipsum dolor sit amet,</b> consectetur adipiscing elit. Cras
+            eu posuere mi, ac ultrices elit. Suspendisse eu neque nec orci
+            pellentesque accumsan.
+          </p>
+          <ul>
+            <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+            <li>Aliquam tincidunt mauris eu risus.</li>
+            <li>Vestibulum auctor dapibus neque.</li>
+            <li>Nunc dignissim risus id metus.</li>
+          </ul>
+          <p>
+            Ut euismod eu velit imperdiet fermentum. Fusce laoreet mi vitae ante
+            posuere ullamcorper.
+          </p>
+          <p>
+            Cras vel orci quam. Duis viverra libero quis magna finibus euismod.
+            Suspendisse potenti. Pellentesque sit amet tempor orci. Praesent
+            elementum lacus et sapien porttitor, et tincidunt turpis egestas.
+            Curabitur congue tincidunt velit, quis rhoncus turpis dapibus at.
+          </p>`,
+      },
+      {
+        id: "34",
+        locked: true,
+        title: "Electrical Fire",
+        subtitle: "In Flight",
+        stars: 1,
+        percentage: 0,
+        description: `
+          <p>
+            <b>Lorem ipsum dolor sit amet,</b> consectetur adipiscing elit. Cras
+            eu posuere mi, ac ultrices elit. Suspendisse eu neque nec orci
+            pellentesque accumsan.
+          </p>
+          <ul>
+            <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+            <li>Aliquam tincidunt mauris eu risus.</li>
+            <li>Vestibulum auctor dapibus neque.</li>
+            <li>Nunc dignissim risus id metus.</li>
+          </ul>
+          <p>
+            Ut euismod eu velit imperdiet fermentum. Fusce laoreet mi vitae ante
+            posuere ullamcorper.
+          </p>
+          <p>
+            Cras vel orci quam. Duis viverra libero quis magna finibus euismod.
+            Suspendisse potenti. Pellentesque sit amet tempor orci. Praesent
+            elementum lacus et sapien porttitor, et tincidunt turpis egestas.
+            Curabitur congue tincidunt velit, quis rhoncus turpis dapibus at.
+          </p>`,
+      },
+    ],
+  },
+];
