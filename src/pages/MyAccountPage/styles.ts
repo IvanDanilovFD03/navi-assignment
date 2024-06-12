f37d9b9658cf37f07c8e9d4ad560ea859727bea1
IvanDanilovFD03 <ivd@empat.tech>
2024-06-12 17:04:53 +0300

    fix: remove height limit of my account page

--- src/pages/MyAccountPage/styles.ts
+++ src/pages/MyAccountPage/styles.ts
@@ -8,8 +8,8 @@ export const styles: SxStyles = {
     height: "100%",
     [basicTheme.breakpoints.up("large")]: {
       pt: 35,
-      height: "100vh",
-      maxHeight: "100vh",
+      // height: "100vh",
+      // maxHeight: "100vh",
     },
   },
 };
